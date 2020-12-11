32f080977a7d87a025fa18cd205e975aaf408889
DenisLabrecque <denis.g.labrecque@gmail.com>
2020-12-11 17:44:17 -0600

    update articles, update animations

--- static/anim.js
+++ static/anim.js
@@ -37,7 +37,7 @@
       //console.log("elementY: " + elementY + ", windowY " + windowY + " value: " + (elementY - windowY) + " " + (elementY - windowY <= 0))
 
       if(element.tagName == "A")
-         console.log("windowY " + windowY + " elementY " + elementY);
+         //console.log("windowY " + windowY + " elementY " + elementY);
 
       if (elementY - windowY <= 0) {
          element.classList.add(AnimatedClass)
@@ -72,3 +72,23 @@
             return window.outerHeight
       }
    }
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+// https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
