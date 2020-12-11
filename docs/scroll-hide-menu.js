796d205f333f692886be3b45806579affb6b1b86
DenisLabrecque <denis.g.labrecque@gmail.com>
2020-12-11 11:52:25 -0600

    add docs folder

---
+++ docs/scroll-hide-menu.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', (event) => {
+    let logo = document.querySelector("#logo-button")
+    let menu = document.querySelector("nav.menu")
+
+    console.log("scroll distance: " + window.pageYOffset)
+ });
